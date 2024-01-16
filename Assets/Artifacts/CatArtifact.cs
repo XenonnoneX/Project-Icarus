cb060fc7342bedea2fe43a38b78e22ff5216b254
Xeno Boecker <xeno.boecker@web.de>
2024-01-16 11:46:25 +0100

    Cat!

---
+++ Assets/Artifacts/CatArtifact.cs
@@ -0,0 +1,17 @@
+﻿public class CatArtifact : Artifact
+{
+    CatInteractable catInteractable;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        catInteractable = FindObjectOfType<CatInteractable>();
+        catInteractable.gameObject.SetActive(false);
+    }
+
+    protected override void EnableArtifact()
+    {
+        base.EnableArtifact();
+        catInteractable.gameObject.SetActive(true);
+    }
+}
